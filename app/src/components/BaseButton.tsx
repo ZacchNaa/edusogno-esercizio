4d9125df52b8165cbb9d9ab96470bd1006af5f54
Zacchaeus <2016.naaz@gmail.com>
2024-04-17 11:06:43 +0000

    add loader and react datetime picker

--- app/src/components/BaseButton.tsx
+++ app/src/components/BaseButton.tsx
@@ -1,11 +1,23 @@
 import { FC } from "react";
+import BaseLoader from "./BaseLoader";
 
 interface BaseButtonProps {
   label: string;
+  disabled?: boolean;
+  loading?: boolean;
 }
 
-const BaseButton: FC<BaseButtonProps> = ({ label }) => {
-  return <button type="submit" className="bg-blue text-white uppercase rounded-xl px-4 py-3">{label}</button>
+const BaseButton: FC<BaseButtonProps> = ({ label, disabled, loading }) => {
+  return (
+    <button
+      type="submit"
+      disabled={disabled}
+      className="bg-blue flex justify-center items-center gap-3 text-white uppercase rounded-xl px-4 py-3"
+    >
+      {loading && <BaseLoader />}
+      {label}
+    </button>
+  );
 };
 
 export default BaseButton;
